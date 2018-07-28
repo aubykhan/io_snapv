42e28380e6eb50ff3e93f7a5114d290ee93119af
Syeda Sana Zehra <sana_sz91@hotmail.com>
2018-07-28 21:04:47 +0500

    first commit - check

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MyApp()); // comment check
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
